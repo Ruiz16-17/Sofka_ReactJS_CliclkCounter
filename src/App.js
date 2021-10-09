0ac2cb6c7e1a92cbae054221749518a51c879852
Ruiz16-17 <83104318+Ruiz16-17@users.noreply.github.com>
2021-10-09 12:58:27 -0500

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
 class App extends Component{
   render(){
     return (
-     
+      <div>
+        This div has been clicked {this.props.clicks} times.
+      </div>
+    );
+  }
 }
 
 export default App;
