38f535511c472fff1929be94ad3bdcbaa33810d3
Sebastian Ruiz Gallego <sebasruigalle62@gmail.com>
2021-10-09 12:44:00 -0500

    Error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 class App extends Component{
   render(){
     return (
-      <div>
-        This div has been clicked {this.props.clicks} times.
-      </div>
-    );
-  }
+     
 }
 
 export default App;
